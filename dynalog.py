50495eb62c86967bbc7798e92d871e099ba9f029
tac4223 <tacspam-github@yahoo.de>
2016-04-17 22:32:32 +0200

    Planobjekt steht soweit

--- dynalog.py
+++ dynalog.py
@@ -7,6 +7,45 @@ Created on Wed Apr 13 09:23:30 2016
 
 import numpy as np
 
+class DynalogMismatchError(Exception):
+    pass
+
+class LeafbankMismatchError(DynalogMismatchError):
+    def __init__(self,plan_uid,beam_number,key,msg="Must be identical"):
+        self.uid = plan_uid
+        self.beam = beam_number
+        self.key = key
+        self.msg = msg
+
+    def __str__(self):
+        return "\nPlan {0}\nBeam {2}\n"\
+        "{1} mismatch between leafbanks A & B: {1} {3}"\
+            .format(self.uid,self.key,self.beam,self.msg)
+
+class BeamMismatchError(DynalogMismatchError):
+    def __init__(self,plan_uid,beam_number,leafbank,key,msg="must be identical"):
+        self.uid = plan_uid
+        self.beam_number = beam_number
+        self.leafbank = leafbank
+        self.key = key
+        self.msg = msg
+
+    def __str__(self):
+        return "\nPlan {0}\nBeam {2}\n"\
+        "{1} mismatch between beam {2} and leafbank {3}: "\
+        "{1} {4}".format(self.uid,self.key,self.beam_number,
+        self.leafbank,self.msg)
+
+class PlanMismatchError(DynalogMismatchError):
+    def __init__(self,plan_uid,key,msg):
+        self.plan_uid = plan_uid
+        self.key = key
+        self.msg = msg
+
+    def __str__(self):
+        return "\nPlan {0}\n{1} mismatch: {2}"\
+        .format(self.plan_uid,self.key,self.msg)
+
 class leafbank:
 
     def __init__(self,filename):
@@ -55,22 +94,6 @@ class leafbank:
         self.leafs_expected = self.raw_data[:,14::4]
         self.leafs_actual = self.raw_data[:,15::4]
 
-class DynalogMismatch(Exception):
-    pass
-
-class LeafbankMismatch(DynalogMismatch):
-    def __init__(self,beam_number,key,msg="Must be identical"):
-        self.beam = beam_number
-        self.key = key
-        self.msg = msg
-
-class BeamMismatch(DynalogMismatch):
-    def __init__(self,beam_number,leafbank,key,msg="must be identical"):
-        self.beam_number = beam_number
-        self.leafbank = leafbank
-        self.key = key
-        self.msg = msg
-
 class beam:
 
     def __init__(self,plan_uid,beam_number,banks):
@@ -85,26 +108,28 @@ class beam:
             for key in self.banks[0].header.keys():
                 if key in ["filename","side"]:
                     if self.banks[0].header[key] == self.banks[1].header[key]:
-                        raise LeafbankMismatch(self.header["beam_number"],
-                        key,"can't be identical for both banks")
+                        raise LeafbankMismatchError(self.header["plan_uid"],
+                            self.header["beam_number"],
+                            key,"can't be identical for both banks")
                 else:
                     if self.banks[0].header[key] != self.banks[1].header[key]:
-                        raise LeafbankMismatch(self.header["beam_number"],key)
+                        raise LeafbankMismatchError(self.header["plan_uid"],
+                                               self.header["beam_number"],key)
 
             for key in self.header.keys():
                     if self.header[key] != self.banks[0].header[key]:
-                        raise BeamMismatch(self.header["beam_number"],
+                        raise BeamMismatchError(self.header["plan_uid"],
+                                           self.header["beam_number"],
                                         self.banks[0].header["side"],key)
                     if self.header[key] != self.banks[1].header[key]:
-                        raise BeamMismatch(self.header["beam_number"],
+                        raise BeamMismatchError(self.header["plan_uid"],
+                                           self.header["beam_number"],
                                         self.banks[1].header["side"],key)
-        except LeafbankMismatch as lbm:
-            print("\nBeam {1}\n{0} mismatch between leafbanks A & B: {0} {2}"\
-            .format(lbm.key,lbm.beam,lbm.msg))
+        except LeafbankMismatchError:
+            self.verified = False
             raise
-        except BeamMismatch as bm:
-            print("\nBeam {1}\n{0} mismatch between beam {1} and leafbank {2}: "\
-            "{0} {3}".format(bm.key,bm.beam_number,bm.leafbank,bm.msg))
+        except BeamMismatchError:
+            self.verified = False
             raise
         else:
             self.verified = True
@@ -116,15 +141,46 @@ class plan:
         self.header = {"plan_uid":plan_uid,"number_of_beams":number_of_beams}
         self.beams = [None for k in range(self.header["number_of_beams"])]
 
+        self.construct_beams(bank_pool)
+
     def construct_beams(self,bank_pool):
         if len(bank_pool) < 2*self.header["number_of_beams"]:
-#            raise PlanMismatch
+            raise PlanMismatchError(self.header["plan_uid"],"beam count",
+                "plan needs {0} leafbanks for beam construction,"\
+                " only {1} were passed."\
+                .format(2*self.header["number_of_beams"],len(bank_pool)))
+        else:
+            beam_nums = [bank.header["beam_number"] for bank in bank_pool]
+            sort_index = np.argsort(beam_nums)
+            for num in range(self.header["number_of_beams"]):
+                self.beams[num] = beam(self.header["plan_uid"],
+                num+1,[bank_pool[sort_index[2*num]],
+                       bank_pool[sort_index[2*num+1]]])
+
+    def verify_plan(self):
+        try:
+            for num in range(self.header["number_of_beams"]):
+                current_beam = self.beams[num]
+                current_beam.verify_beam()
+                if current_beam.header["beam_number"] != num+1:
+                    raise PlanMismatchError(self.header["plan_uid"],
+                    "beam assignment","beam at position {0} of beam list"\
+                    " identifies as beam {1} instead of beam {2}."\
+                    .format(num,current_beam.header["beam_number"],num+1))
+        except PlanMismatchError:
+            self.verified = False
+            raise
+        else:
+            self.verified = True
 
 
 
 
 if __name__ == "__main__":
-    dyn1 = leafbank("A20151125181338_Test40.dlg")
-    dyn2 = leafbank("A20151125181338_Test40.dlg")
+    dyn1 = leafbank("A1.dlg")
+    dyn2 = leafbank("B1.dlg")
+    dyn3 = leafbank("A2.dlg")
+    dyn4 = leafbank("B2.dlg")
 
-    beam1 = beam("derp",1,[dyn1,dyn2])
+#    beam1 = beam("derp",1,[dyn1,dyn2])
+    plan1 = plan("derp",2,[dyn1,dyn2,dyn3,dyn4])
